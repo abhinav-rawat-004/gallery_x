68c6430386abaaf6825480ec7a1e6719ee3428fb
Khuza1ma <khuzaimanrko@gmail.com>
2025-12-12 16:44:40 +0530

    Fix: Stored image path as a List<int>

--- lib/app/data/models/image.dart
+++ lib/app/data/models/image.dart
@@ -1,32 +1,31 @@
-
 import 'dart:typed_data';
 
 class Image {
-    String name;
-    Uint8List path;
-    String type;
-    String date;
+  String name;
+  Uint8List path;
+  String type;
+  String date;
 
-    Image({
-        required this.name,
-        required this.path,
-        required this.type,
-        required this.date,
-    });
+  Image({
+    required this.name,
+    required this.path,
+    required this.type,
+    required this.date,
+  });
 
-    factory Image.fromMap(Map<String, dynamic> map) {
-      return Image(
-        name: map["name"],
-        path: map["path"],
-        type: map["type"],
-        date: map["date"],
-      );
-    }
+  factory Image.fromMap(Map<String, dynamic> map) {
+    return Image(
+      name: map["name"],
+      path: Uint8List.fromList(List<int>.from(map["path"])),
+      type: map["type"],
+      date: map["date"],
+    );
+  }
 
-    Map<String, dynamic> toMap() => {
-        "name": name,
-        "path": path,
-        "type": type,
-        "date": date,
-    };
+  Map<String, dynamic> toMap() => {
+    "name": name,
+    "path": path.toList(),
+    "type": type,
+    "date": date,
+  };
 }
