d84829f6d819b6c243b566f41688f7d14150d193
abhinav <abhinavrawat8792@gmail.com>
2025-12-15 18:32:10 +0530

    Added little details on settings

--- lib/app/modules/settings/controllers/settings_controller.dart
+++ lib/app/modules/settings/controllers/settings_controller.dart
@@ -1,9 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:gallery_x/app/controller/theme_controller.dart';
 import 'package:get/get.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SettingsController extends GetxController {
+  RxString appName = ''.obs;
+  RxString appVersion = ''.obs;
+  RxBool darkMode = false.obs;
+
+  ThemeController appController = Get.find<ThemeController>();
+
+  @override
+  void onInit() async {
+    super.onInit();
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    appName(packageInfo.appName);
+    appVersion(packageInfo.version);
+    print(appController.storage.read('themeMode'));
+    if (appController.storage.read('themeMode') == ThemeMode.dark.name) {
+      darkMode(true);
+      print(darkMode);
+    } else {
+      darkMode(false);
+      print(darkMode);
+    }
+  }
+
   void toggleTheme() {
-    ThemeController appController = Get.find<ThemeController>();
     try {
       appController.toggleTheme();
     } catch (error) {
--- lib/app/modules/settings/views/settings_view.dart
+++ lib/app/modules/settings/views/settings_view.dart
@@ -7,24 +7,51 @@ class SettingsView extends GetView<SettingsController> {
   const SettingsView({super.key});
 
   @override
+  SettingsController get controller => Get.put(SettingsController());
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(8),
       height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: [
-          Center(child: Text('Appname')),
-          Text('App Version'),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 1,
-              itemBuilder: (context, index) => GestureDetector(
-                onTap: () => controller.toggleTheme(),
-                child: ListTile(title: Text('Theme $index ')),
+      child: Obx(
+        () => Column(
+          children: [
+            ListTile(
+              title: Text(
+                'App',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              trailing: Text(
+                controller.appName().toUpperCase(),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
               ),
             ),
-          ),
-        ],
+            ListTile(
+              title: Text(
+                'App Version',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              trailing: Text(
+                controller.appVersion(),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Dark Mode',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              trailing: Switch(
+                value: controller.darkMode(),
+                onChanged: (value) {
+                  controller.darkMode(value);
+                  controller.toggleTheme();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
